bbd1ae287b4b1e414e173222a95a68c84a7cf08b
Lidia <lidiahryma22@gmail.com>
2023-07-22 19:02:55 +0300

    next.c

--- logic.py
+++ logic.py
@@ -3,13 +3,15 @@ from hw6 import Name, Phone, Birthday, Record, AddressBook
 if __name__ == "__main__":
     ab = AddressBook()
     name = Name("Bill Gates")
-    name.value = "Bill Gates"
+
     record = Record(name)
     phone1 = Phone("2617627334")
     print(name.value)
     print(phone1.value)
+
     ab.add_record(record)
     record.add_phone(phone1)
+
     birthday = Birthday("22/07/2023")
     record.birthday = birthday
     print(record.days_to_birthday())
